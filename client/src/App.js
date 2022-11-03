c6060268095dc3cc09c7a0dd5b1b820719919179
Suagrcothe <ifeanyivalentine82@gmail.com>
2022-11-03 11:34:01 +0100

    react router fix

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,26 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Profile/>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
-
 export default App;
